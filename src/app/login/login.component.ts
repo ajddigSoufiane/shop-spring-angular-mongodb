6d9317b75fc866474a9e110e59afd3061e4ab341
sousoucat <ajddig2013@gmail.com>
2018-01-30 23:53:24 +0000

    @soufianeAjddig: Sign Up task

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,34 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../services/log-in.service';
-import { Router } from '@angular/router';
+import {Router} from "@angular/router";
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
-
+import { User } from '../model/user';
+ 
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
     styleUrls:['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    valForm: FormGroup;
-    constructor(private loginservice :  LoginService,fb:FormBuilder,
-        private router: Router) { 
-            this.valForm = fb.group({
-                'username': [null, Validators.required],
+    signUp:boolean;
+    loginForm: FormGroup;
+    InscriptionForm: FormGroup;
+    constructor(private loginservice :  LoginService,fb:FormBuilder,private router: Router) { 
+            this.loginForm = fb.group({
+                'email': [null, Validators.required],
                 'password': [null, Validators.required]
               });
-          
+              this.InscriptionForm = fb.group({
+                'email': [null, Validators.required],
+                'username': [null, Validators.required],
+                'password': [null, Validators.required],
+                'confirmPassword':[null, Validators.required],
+              });
          }
 
     ngOnInit() { }
-    login(event, email, password) {
+    login(event) {
         event.preventDefault();
-        this.loginservice.login(email, password)
+        if(this.loginForm.valid && this.loginForm.value){
+        this.loginservice.login(this.loginForm.value.email, this.loginForm.value.password)
             .subscribe(() => {
-                this.router.navigate(['/home']);
-            }, this.handleError);
+                //this.router.navigate(['/home']);
+            }, this.handleError);}
     }
-
+    inscription(){
+        if(this.InscriptionForm.valid && this.InscriptionForm.value){
+            let user:User=new User()
+            Object.assign(user,this.InscriptionForm.value)
+             this.loginservice.inscription(user)
+                .subscribe(() => {
+                    this.signUp=true;
+                }, this.handleError);}
+    }
     handleError(error) {
-        this.router.navigate(['/home']);
+       // this.router.navigate(['/home']);
     }
 }
--- src/app/services/log-in.service.ts
+++ src/app/services/log-in.service.ts
@@ -3,23 +3,50 @@ import {Injectable} from "@angular/core";
 import {Response} from "@angular/http";
 import { Headers, Http } from '@angular/http';
 import "rxjs/Rx";
-
+import { User } from "../model/user"; 
+import {RequestOptions} from "@angular/http";
 
 @Injectable()
 export class LoginService {
-
+    body: string;
     constructor(private http:Http) {}
 
-    login(email, password):Observable<Response> {
-        let loginRequest = JSON.stringify({email: email, password: password});
+    // login(email, password):Observable<Response> {
+    //     let loginRequest = JSON.stringify({email: email, password: password});
+    //     let headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/json'});
+
+    //     return this.http.post('http://localhost:9091/api/login', loginRequest, { headers: headers })
+    //                     .do(resp => {
+    //                         localStorage.setItem('jwt', resp.headers.get('x-auth-token'));
+    //                     });
+    // }
+
+    login(email, password): Observable<Response> {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        headers.set('authorization', 'Bearer.');
+        headers.set('accept', 'application/json');
+        let options = new RequestOptions({headers: headers});
+    
+        this.body = JSON.stringify({
+            email: email,
+          password: password,
+        });
+    
+        return this.http.post('http://localhost:9091/api/login', this.body, options)
+          .map(resp => {
+            localStorage.setItem('token', resp.headers.get('x-auth-token'));
+          })
+          .catch(this.handleError);
+      }
+    inscription(user:User){
+        let insriptionRequest = JSON.stringify({user});
         let headers = new Headers({'Content-Type': 'application/json', 'Accept': 'application/json'});
 
-        return this.http.post('http://localhost:9091/api/login', loginRequest, { headers: headers })
+        return this.http.post('http://localhost:9091/api/users', insriptionRequest, { headers: headers })
                         .do(resp => {
-                            localStorage.setItem('jwt', resp.headers.get('x-auth-token'));
+                            //localStorage.setItem('jwt', resp.headers.get('x-auth-token'));
                         });
     }
-
     logout():void {
         localStorage.removeItem('jwt');
     }
